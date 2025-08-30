f525b3ff79321cd749b665ce6c32a9c0abf48981
GyeongBin Min <min06150315@gmail.com>
2025-08-30 20:07:18 +0900

    Update 10699.java

--- BAEKJOON/10699.java
+++ BAEKJOON/10699.java
@@ -4,7 +4,7 @@ import java.text.SimpleDateFormat;
 class Main {
 	public static void main(String[] args) {
 		Date d = new Date();
-		SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-DD");
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		System.out.println(sdf.format(d));
 	}
 }
